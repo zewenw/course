3e44f7010f100e34ce9547c42982cb01ad6b986a
zewenw <wangzewen8527@163.com>
2023-02-13 20:28:59 +0000

    SOA group work two

---
+++ algorithm/group_work.py
@@ -0,0 +1,28 @@
+import math
+
+
+def new_sort(a, i, j):
+    print(a)
+    if i == j:
+        return
+    elif i + 1 == j:
+        if a[i] > a[j]:
+            a[i], a[j] = a[j], a[i]
+            # print(a)
+    elif i + 2 == j:
+        if a[i] > a[i + 1]:
+            a[i], a[i + 1] = a[i + 1], a[i]
+        if a[i + 1] > a[j]:
+            a[i + 1], a[j] = a[j], a[i + 1]
+        if a[i] > a[i + 1]:
+            a[i], a[i + 1] = a[i + 1], a[i]
+        # print(a)
+    else:
+        k = math.floor((j - i + 1) / 4)
+        new_sort(a, i, j - k)
+        new_sort(a, i + k, j)
+        new_sort(a, i, j - k)
+
+
+a = [0, 8, 8, 7, 7]
+new_sort(a, 0, len(a) - 1)
